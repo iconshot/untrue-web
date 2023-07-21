11051c74de38d95e11c15511a3b7f0a1317ce5e5
Jose Carlos Ramírez <jc@iconshot.com>
2023-07-20 21:40:47 -0500

    fix Head bug

--- src/Head.js
+++ src/Head.js
@@ -6,7 +6,7 @@ export class Head extends Component {
   constructor(props) {
     super(props);
 
-    this.tree = null;
+    this.tree = new Tree(document.head);
 
     this.on("render", this.handleRender);
     this.on("unmount", this.handleUnmount);
@@ -14,10 +14,6 @@ export class Head extends Component {
 
   handleRender = () => {
     const { children } = this.props;
-
-    const head = document.head;
-
-    this.tree = new Tree(head);
 
     this.tree.mount(new Node(children));
   };
